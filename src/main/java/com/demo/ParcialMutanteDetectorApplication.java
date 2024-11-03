1b6a44d68b358154f8be82474a030d17b1021cbd
Davidan-root <Davidan2017@gmail.com>
2024-11-03 00:34:16 -0300

    first commit

---
+++ src/main/java/com/demo/ParcialMutanteDetectorApplication.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialMutanteDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( ParcialMutanteDetectorApplication.class, args);
+
+		System.out.println("Aplcacion en Funcionamiento");
+	}
+
+}
